3d2d3aff3b62516991d97826596aa945a6282111
Mogens Yde-Andersen <mogensydeandersen@gmail.com>
2015-02-22 17:58:46 +0100

    ProgrammingAssignment 2 cachematrix.R solution
    my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions "makeCacheMatrix" and "cacheSolve" in kombination calculates
+## the inverse matrix of a matrix, but in computational rational way.
+## Since calculating af matrix and a inverse matrix demands computational
+## power, the second function "cacheSolve" avoids to recalculate, if a
+## user has not changed the vlue of the input matrix. In this case the
+## second function "only" retunrs the cached value
 
-## Write a short comment describing this function
+## The makeCacheMatrix set and gets the input matrix and set og gets the solve
+## function (= inverse matrix function)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m<-NULL
+      set<-function(y){
+              x<<-y
+              m<<-NULL
+              }
+      get<-function() x
+      setmatrix<-function(solve) m<<- solve
+      getmatrix<-function() m
+      list(set=set, get=get,
+      setmatrix=setmatrix,
+      getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## The cacheSolve function is the energisafer function. If user has not
+## inputted a new matrix, cacheColve gets cached inverted matrix data.
+##If user changed input matrix, cacheSolve recalculates inverted matrix data.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x=matrix(), ...) {
+      m<-x$getmatrix()
+      if(!is.null(m)){
+              message("getting cached inverted matrix data")
+              return(m)
+              }
+      matrix<-x$get()
+      m<-solve(matrix, ...)
+      x$setmatrix(m)
+      m
 }
